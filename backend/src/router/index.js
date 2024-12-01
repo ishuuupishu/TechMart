d57e63306b64ab7e766e7e9772b19d650a525cc6
ishuuupishu <ishuenii134@gmail.com>
2024-12-02 00:45:49 +0600

    update index.js

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue"; 
 import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
+
 
 const routes = [
     {
@@ -18,7 +20,12 @@ const routes = [
         path: '/request-password',
         name: 'requestPassword',
         component: RequestPassword,
-      },
+    },
+    // {
+    //     path: '/reset-password',
+    //     name: 'resetPassword',
+    //     component: ResetPassword,
+    // },
 ];
 
 const router = createRouter({
