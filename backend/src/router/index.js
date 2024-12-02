ad5564925f0d70e982b8f21dee268f55ea31f160
ishuuupishu <ishuenii134@gmail.com>
2024-12-02 11:15:20 +0600

    update views folder..add orders section..cutomers sections..and set the dashboard page

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -3,34 +3,156 @@ import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue"; 
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
+import store from "../store";
+import AppLayout from '../components/AppLayout.vue';
 
 
+
+// const routes = [
+//     {
+//         path: '/' ,
+//         name: 'dashboard' ,
+//         component: Dashboard
+//     },
+//     {
+//         path: '/login' ,
+//         name: 'login' ,
+//         component: Login
+//     },
+//     {
+//         path: '/request-password',
+//         name: 'requestPassword',
+//         component: RequestPassword,
+//     },
+//     {
+//         path: '/reset-password',
+//         name: 'resetPassword',
+//         component: ResetPassword,
+//     },
+// ];
+
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes
+// })
+
+// export default router;
 const routes = [
     {
-        path: '/' ,
-        name: 'dashboard' ,
-        component: Dashboard
+      path: '/',
+      redirect: '/app'
     },
     {
-        path: '/login' ,
-        name: 'login' ,
-        component: Login
+      path: '/app',
+      name: 'app',
+      redirect: '/app/dashboard',
+      component: AppLayout,
+      meta: {
+        requiresAuth: true
+      },
+      children: [
+        {
+          path: 'dashboard',
+          name: 'app.dashboard',
+          component: Dashboard
+        },
+        {
+          path: 'products',
+          name: 'app.products',
+          component: Products
+        },
+        {
+          path: 'users',
+          name: 'app.users',
+          component: Users
+        },
+        {
+          path: 'customers',
+          name: 'app.customers',
+          component: Customers
+        },
+        {
+          path: 'customers/:id',
+          name: 'app.customers.view',
+          component: CustomerView
+        },
+        {
+          path: 'orders',
+          name: 'app.orders',
+          component: Orders
+        },
+        {
+          path: 'orders/:id',
+          name: 'app.orders.view',
+          component: OrderView
+        },
+        {
+          path: '/report',
+          name: 'reports',
+          component: Report,
+          meta: {
+            requiresAuth: true
+          },
+          children: [
+            {
+              path: 'orders/:date?',
+              name: 'reports.orders',
+              component: OrdersReport
+            },
+            {
+              path: 'customers/:date?',
+              name: 'reports.customers',
+              component: CustomersReport
+            }
+          ]
+        },
+      ]
     },
     {
-        path: '/request-password',
-        name: 'requestPassword',
-        component: RequestPassword,
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
     },
-    // {
-    //     path: '/reset-password',
-    //     name: 'resetPassword',
-    //     component: ResetPassword,
-    // },
-];
-
-const router = createRouter({
+    {
+      path: '/request-password',
+      name: 'requestPassword',
+      component: RequestPassword,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/reset-password/:token',
+      name: 'resetPassword',
+      component: ResetPassword,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/:pathMatch(.*)',
+      name: 'notfound',
+      component: NotFound,
+    }
+  ];
+  
+  const router = createRouter({
     history: createWebHistory(),
     routes
-})
-
-export default router;
+  })
+  
+  router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+      next({name: 'login'})
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+      next({name: 'app.dashboard'})
+    } else {
+      next();
+    }
+  
+  })
+  
+  export default router;
--- backend/src/store/index.js
+++ backend/src/store/index.js
@@ -1,13 +1,13 @@
 import {createStore} from "vuex";
-
+import state from './state'
+import * as actions from './actions'
+import * as mutations from './mutations'
 
 const store = createStore({
-  state: {
-    test: '12345678'
-  },
+  state,
   getters: {},
-  actions: {},
-  mutations: {},
+  actions,
+  mutations,
 })
 
 export default store
